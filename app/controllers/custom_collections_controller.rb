1ab3814db12f3dd5f6aab514385e0bfcd28975a6
Reade Harris <services@followthewolf.com>
2011-05-30 19:31:10 -0400

    Proper custom collection titles.

--- app/controllers/custom_collections_controller.rb
+++ app/controllers/custom_collections_controller.rb
@@ -1,2 +1,6 @@
 class CustomCollectionsController < FrontController
+  def show
+    @custom_collection = CustomCollection.find( params[ :id ] )
+    @title             = @custom_collection.name
+  end
 end
